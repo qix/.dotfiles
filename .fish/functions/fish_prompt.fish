68034ae2cfd579b03b6c2929acc26fde8bc4ff4a
Josh Yudaken <j@yud.co.za>
2013-12-02 10:38:07 -0800

    Update fish_prompt with ~/.name

--- .fish/functions/fish_prompt.fish
+++ .fish/functions/fish_prompt.fish
@@ -3,7 +3,7 @@ function fish_prompt --description 'Write out the prompt'
     set stat $status
 
 
-    # Just calculate these once, to save a few cycles when displaying the prompt
+    # Just calculate these once, to save a few cycles when displaying the prompt (and allow override)
     if not set -q __fish_prompt_hostname
         set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
     end
@@ -12,13 +12,19 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_color_normal (set_color normal)
         set -g __fish_color_blue (set_color -o blue)
         set -g __fish_color_grey (set_color white)
-        set -g __fish_color_prompt (set_color --bold --underline white)
+        set -g __fish_color_prompt (set_color black)
+    end
+
+    if test -e ~/.name
+      set __fish_name (cat ~/.name)
+    else
+      set __fish_name "$USER@$__fish_prompt_hostname"
     end
 
     #Set the color for the status depending on the value
     set __fish_status ""
     if test $stat -gt 0
-        set __fish_status (set_color red)"($stat)"
+        set __fish_status (set_color red)" ($stat)"
     end
 
     switch $USER
@@ -33,7 +39,7 @@ function fish_prompt --description 'Write out the prompt'
             end
         end
 
-        printf '%s@%s %s%s%s# ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_color_normal"
+        printf '%s %s%s%s# ' $__fish_name "$__fish_prompt_cwd" (prompt_pwd) "$__fish_color_normal"
 
         case '*'
 
@@ -41,9 +47,8 @@ function fish_prompt --description 'Write out the prompt'
             set -g __fish_prompt_cwd (set_color $fish_color_cwd)
         end
 
-        printf '%s[%s] %s%s@%s %s%s %s\f\r%s>%s '  \
-          $__fish_color_grey (date "+%H:%M:%S") \
-          $__fish_color_blue $USER $__fish_prompt_hostname \
+        printf '%s%s %s%s%s%s$%s '  \
+          $__fish_color_blue $__fish_name \
           $__fish_prompt_cwd  (pwd) \
           $__fish_status \
           $__fish_color_prompt $__fish_color_normal
